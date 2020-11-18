17ea2d3fda2288333006197f31e8ec66cb222b61
Ghizlane AROUSSI <ghizlane.aroussi@gmail.com>
2020-11-18 15:14:58 +0100

    submission 2 after review

--- cand-c2-project-master/Exercise_1/main.tf
+++ cand-c2-project-master/Exercise_1/main.tf
@@ -17,7 +17,7 @@ resource "aws_instance" "Udacity_T2" {
   ami = "ami-0323c3dd2da7fb37d"
   instance_type = "t2.micro"
   tags = {
-    name = "Udacity project 2"
+    Name = "Udacity project 2"
   }
   subnet_id = var.public_subnet
 }
@@ -29,7 +29,7 @@ resource "aws_instance" "Udacity_M4" {
   ami = "ami-0323c3dd2da7fb37d"
   instance_type = "m4.large"
   tags = {
-    name = "Udacity project 2"
+    Name = "Udacity project 2"
   }
   subnet_id = var.public_subnet
 }
